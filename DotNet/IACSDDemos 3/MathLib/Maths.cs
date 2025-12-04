581c435ceeb3653ff6b9386d4804ea79ebf2294b
AkshayKothule <akshaykothule2002@gmail.com>
2025-12-04 23:45:04 +0530

    update

---
+++ DotNet/IACSDDemos 3/MathLib/Maths.cs
@@ -0,0 +1,17 @@
+﻿namespace MathLib
+{
+    [Serializable]
+    public class Maths
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+}
